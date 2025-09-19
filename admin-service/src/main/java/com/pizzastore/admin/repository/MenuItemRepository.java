12b3f982e2f1d1e84ae77fab0fad61618627508d
Gopi Lakke <106574401+gopilakke@users.noreply.github.com>
2025-09-19 21:10:39 +0530

    Add files via upload

---
+++ admin-service/src/main/java/com/pizzastore/admin/repository/MenuItemRepository.java
@@ -0,0 +1,1 @@
+package com.pizzastore.admin.repository; import com.pizzastore.admin.entity.MenuItem; import org.springframework.data.jpa.repository.JpaRepository; public interface MenuItemRepository extends JpaRepository<MenuItem, Long> {}
