da53a17105bde881a70589b773262422a7ce2ad2
Gopi Lakke <106574401+gopilakke@users.noreply.github.com>
2025-09-19 21:09:52 +0530

    Add files via upload

---
+++ user-service/src/main/java/com/pizzastore/user/repository/MenuItemRepository.java
@@ -0,0 +1,1 @@
+package com.pizzastore.user.repository; import com.pizzastore.user.entity.MenuItem; import org.springframework.data.jpa.repository.JpaRepository; import java.util.List; public interface MenuItemRepository extends JpaRepository<MenuItem, Long> { List<MenuItem> findByCategoryIgnoreCase(String category); List<MenuItem> findByNameContainingIgnoreCase(String name); }
---
+++ user-service/src/main/java/com/pizzastore/user/repository/OrderRepository.java
@@ -0,0 +1,1 @@
+package com.pizzastore.user.repository; import com.pizzastore.user.entity.OrderEntity; import org.springframework.data.jpa.repository.JpaRepository; import java.util.List; public interface OrderRepository extends JpaRepository<OrderEntity, Long> { List<OrderEntity> findByUserId(Long userId); }
